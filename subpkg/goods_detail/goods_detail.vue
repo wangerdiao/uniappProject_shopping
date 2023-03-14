db875882dc8af6a67f8cc687c7f6eecce03c6691
王二雕 <2504204375@qq.com>
2023-03-14 22:51:45 +0800

    商品详情页完成

--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -1,6 +1,34 @@
 <template>
-  <view>
-    goods_detail
+  <view class="container">
+    <!-- 轮播图区域 -->
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+      <swiper-item v-for="(item,i) in goodsDetail.pics" :key="i">
+        <image :src="item.pics_big" mode="" @click="preview(i)"></image>
+      </swiper-item>
+    </swiper>
+    <!-- 商品信息区域 -->
+    <view class="goodsArea" v-if="goodsDetail.goods_price">
+      <!-- 商品价格区域 -->
+      <view class="price">￥{{goodsDetail.goods_price}}</view>
+      <!-- 商品详细信息区域 和收藏按钮 -->
+      <view class="goodsInfo">
+        <view class="info">
+          {{goodsDetail.goods_name}}
+        </view>
+        <view class="icon">
+          <uni-icons type="star" size="30" color="gray"></uni-icons>
+          <text class="iconText">收藏</text>
+          <uni-icons type="star-filled" v-show="false"></uni-icons>
+        </view>
+      </view>
+      <!-- 免运费区域 -->
+      <view class="yunfei">快递:免运费</view>
+      <rich-text :nodes="goodsDetail.goods_introduce"></rich-text>
+    </view>
+    <!-- 商品导航组件 -->
+   <view class="goods_nav">
+      <uni-goods-nav :fill="true" :options="options" :button-group="buttonGroup" @click="onClick" @buttonClick="buttonClick" />
+   </view>
   </view>
 </template>
 
@@ -8,12 +36,116 @@
   export default {
     data() {
       return {
-        
+        goodsDetail:{},//商品详情数据
+        //左侧按钮组的配置对象
+        options:[{
+          icon:'shop',
+          text:'店铺'
+        },{
+          icon:'cart',
+          text:'购物车',
+          info:2
+        }],
+        //右侧按钮配置对象
+        buttonGroup:[
+          {
+            text:'加入购物车',
+            backgroundColor:'#ff0000',
+            color:'#fff'
+          },
+          {
+            text:'立即购买',
+            backgroundColor:'#ffa200',
+            color:'#fff'
+          }
+        ]
       };
+    },
+    onLoad(options) {
+      console.log(options);
+      const goods_id = options.goods_id
+      this.getGoodsDetail(goods_id)
+    },
+    methods:{
+      async getGoodsDetail(goods_id) {
+        const {data} = await uni.$http.get('/goods/detail',{goods_id})
+        if(data.meta.status == 200) {
+          data.message.goods_introduce = data.message.goods_introduce.replace(/<img /g,'<img style="display:block;"').replace(/webp/g,'jpg')
+          this.goodsDetail = data.message
+        }else {
+          return uni.$showMsg()
+        }
+      },
+      //大图预览
+      preview(i) {
+        uni.previewImage({
+          current:i,
+          urls:this.goodsDetail.pics.map(x => x.pics_big)
+        })
+      },
+      //导航组件click事件
+      onClick(e) {
+        console.log(e);
+        if(e.content.text === '购物车') {
+          console.log(1);
+          uni.switchTab({
+            url:'/pages/car/car'
+          })
+        }
+      },
+      //导航组件按钮事件@buttonClick
+      buttonClick() {
+        
+      }
     }
   }
 </script>
 
 <style lang="scss">
-
+  .container{
+    background-color: #fff;
+  }
+swiper {
+  height: 750rpx;
+  image{
+    width: 100%;
+    height: 100%;
+  }
+}
+.goodsArea {
+  display: flex;
+  flex-direction: column;
+  margin: 5px 10px;
+  .price {
+    color: red;
+    font-size: 20px
+  }
+  .goodsInfo {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    margin: 8px 0;
+    .info {
+      font-size: 14px;
+      width: 80%;
+      border-right:1px solid #efefef ;
+      font-weight: 700;
+    }
+    .icon {
+      display: flex;
+      flex-direction: column;
+      color: gray;
+    }
+  }
+  .yunfei {
+    color:#a1a1a1;
+    font-size: 12px;
+    margin-bottom: 10px;
+  }
+}
+.goods_nav {
+  position: sticky;
+  bottom: 0;
+  z-index: 999;
+}
 </style>

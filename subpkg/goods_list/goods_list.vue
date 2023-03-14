429312f209aea703184821c902a4218eee436b02
王二雕 <2504204375@qq.com>
2023-03-14 21:19:39 +0800

    商品列表详情完成

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,8 @@
 <template>
-  <view>
-    goods_list
+  <view class="goodsList">
+   <view v-for="item in goodsList" :key="item.goods_id" @click="gotoDetail(item)">
+     <my-goods :item="item"></my-goods>
+   </view>
   </view>
 </template>
 
@@ -8,12 +10,71 @@
   export default {
     data() {
       return {
-        
-      };
+        queryObj:{ //请求携带的参数
+          query:'',
+          cid:'',
+          pagenum:1,
+          pagesize:20
+        },
+        goodsList:[], // 商品列表数据
+        total:0,//数据总数量，用来实现分页
+        deafult:'https://www.somode.com/uploadimg/image/20230129/20230129150835_59024.jpg',
+        isLoading:false, //节流阀
+      };  
+    },
+    onLoad(options) {
+      this.queryObj.query = options.query || ''
+      this.queryObj.cid = options.cid || ''
+      this.getGoodsList()
+    },
+    methods:{
+      //获取商品列表数据
+      async getGoodsList(cb) {
+        this.isLoading = true
+        const {data} = await uni.$http.get('/goods/search',this.queryObj)
+        //关闭下拉刷新的回调
+        cb && cb()
+        if(data.meta.status == 200) {
+          this.goodsList =[...this.goodsList,...data.message.goods]
+          this.total = data.message.total
+          this.isLoading = false
+        }else {
+          this.isLoading = false
+          return uni.$showMsg()
+        }
+      },
+      //跳转到商品详情
+      gotoDetail(item) {
+        console.log(item);
+        uni.navigateTo({
+          url:'/subpkg/goods_detail/goods_detail?goods_id='+ item.goods_id
+        })
+      }
+    },
+    //下拉触底
+    onReachBottom() {
+      if(this.queryObj.pagenum*this.queryObj.pagesize>=this.total)
+      return uni.$showMsg('数据加载完毕')
+      if(!this.isLoading) {
+        this.queryObj.pagenum += 1
+        this.getGoodsList()
+      }
+    },
+    // 上拉刷新
+    onPullDownRefresh() {
+      this.queryObj.pagenum = 1
+      this.total = 0
+      this.goodsList = []
+      this.isLoading = false
+      this.getGoodsList(() => uni.stopPullDownRefresh())
     }
+    
   }
 </script>
 
 <style lang="scss">
-
+.goodsList {
+  margin-top: 5px;
+  background-color: #fff;
+}
 </style>
---
+++ uni_modules/my-goods/components/my-goods/my-goods.vue
@@ -0,0 +1,64 @@
+<template>
+  <view class="goodsList-item" >
+    <!-- 商品左侧图片区域 -->
+    <view class="img">
+      <image :src="item.goods_small_logo || deafult" mode="" ></image>
+    </view>
+    <!-- 右侧信息区域 -->
+    <view class="itemNameAndPrice">
+      <view class="name">{{item.goods_name}}</view>
+      <view class="goodsInfo">
+        <view class="price">
+          ￥{{item.goods_price.toFixed(2)}}
+        </view>
+      </view>
+    </view>
+  </view>
+
+</template>
+<script>
+  export default {
+    props:{
+      item:{
+        type:Object,
+        deafult:{}
+      }
+    },
+    data() {
+      return {
+        deafult:'https://www.somode.com/uploadimg/image/20230129/20230129150835_59024.jpg',
+      }
+    }
+  }
+</script>
+<style lang="scss">
+  .goodsList-item {
+    display: flex;
+    justify-content: space-around;
+    border-bottom: 1px solid #f0f0f0;
+  .img {
+   display: block;
+   margin-bottom: 8px;
+   margin-right: 5px;
+   padding: 10px 5px;
+    image {
+      width: 100px;
+      height: 100px;
+    }
+  }
+  .itemNameAndPrice {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    .name {
+      font-style: 13px;
+    }
+    .goodsInfo {
+      .price {
+        font-size: 16px;
+        color: red;
+      }
+    }
+  }
+  }
+</style>
